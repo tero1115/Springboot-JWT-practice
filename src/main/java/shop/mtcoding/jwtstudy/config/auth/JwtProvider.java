a034ea3c532739a07b91b9a84ed66c7b8a0137fc
tero1115 <limjisang1115@gmail.com>
2023-03-20 17:58:04 +0900

    JWT 인증 처리 완료

---
+++ src/main/java/shop/mtcoding/jwtstudy/config/auth/JwtProvider.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.jwtstudy.config.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import shop.mtcoding.jwtstudy.model.User;
+
+import java.util.Date;
+
+public class JwtProvider {
+
+    private static final String SUBJECT = "jwtstudy";
+    private static final int EXP = 1000 * 60 * 60;
+    public static final String TOKEN_PREFIX = "Bearer "; // 스페이스 필요함
+    public static final String HEADER = "Authorization";
+    private static final String SECRET = "메타코딩";
+
+    public static String create(User user) {
+        String jwt = JWT.create()
+                .withSubject(SUBJECT)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
+                .withClaim("id", user.getId())
+                .withClaim("role", user.getRole())
+                .sign(Algorithm.HMAC512(SECRET));
+        return TOKEN_PREFIX + jwt;
+    }
+
+    public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512("메타코딩"))
+                .build().verify(jwt);
+        return decodedJWT;
+    }
+}
